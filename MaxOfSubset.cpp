b2a8b4d82a66b01fa5957bdf73f35068de23d54a
Sanju <anutanwer12@gmail.com>
2022-01-09 22:24:03 +0530

    Solve one more question

---
+++ MaxOfSubset.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+  public:
+    //Function to find maximum of each subarray of size k.
+    vector <int> max_of_subarrays(int *arr, int n, int k)
+    {
+        // your code here
+       vector<int>ans;
+       
+       multiset<int, greater<int>>set;
+       
+       for(int i = 0; i < k; i++){
+           set.insert(arr[i]);
+       }
+    
+       ans.push_back(*set.begin());
+       
+       for(int i = k; i < n; i++){
+           
+           auto it = set.find(arr[i-k]);
+           
+           set.erase(it);
+           
+           set.insert(arr[i]);
+           
+           ans.push_back(*set.begin());
+           
+       }
+       return ans;
+    }
+};
