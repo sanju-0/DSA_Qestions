8246b2b4d18a3c9c732e8ac57838372464a97bf8
Sanju <anutanwer12@gmail.com>
2022-01-22 14:47:16 +0530

    Updated Files

--- checking.cpp
+++ checking.cpp
@@ -1,50 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution
 {
-    public:
+public:
     int row = 0, col = 0;
-    vector<int>dir1 = {0,-1,0,1,0};
-    vector<int>dir2 = {-1,-1,1,1,-1};
-    //Function to find unit area of the largest region of 1s.
-    int dfs(vector<vector<int>>& grid, vector<vector<int>>& visited, int r, int c){
+    vector<int> dir1 = {0, -1, 0, 1, 0};
+    vector<int> dir2 = {-1, -1, 1, 1, -1};
+    // Function to find unit area of the largest region of 1s.
+    int dfs(vector<vector<int>> &grid, vector<vector<int>> &visited, int r, int c)
+    {
         visited[r][c] = 1;
         int ctr = 0;
-        queue<pair<int,int>>q;
-        q.push({r,c});
-        while(!q.empty()){
+        queue<pair<int, int>> q;
+        q.push({r, c});
+        while (!q.empty())
+        {
             int r1 = q.front().first, c1 = q.front().second;
             q.pop();
-            for(int i=0; i<4; i++){
-                int x = r1 + dir1[i], y = c1 + dir1[i+1];
-                if((x >= 0 and x < row ) and (y >= 0 and y < col) and grid[x][y] == 1 and visited[x][y] == 0){
-                    q.push({x,y});
+            for (int i = 0; i < 4; i++)
+            {
+                int x = r1 + dir1[i], y = c1 + dir1[i + 1];
+                if ((x >= 0 and x < row) and (y >= 0 and y < col) and grid[x][y] == 1 and visited[x][y] == 0)
+                {
+                    q.push({x, y});
                     visited[x][y] = 1;
                 }
             }
-            for(int i=0; i<4; i++){
-                int x = r1 + dir2[i], y = c1 + dir2[i+1];
-                if((x >= 0 and x < row ) and (y >= 0 and y < col) and grid[x][y] == 1 and visited[x][y] == 0){
-                    q.push({x,y});
+            for (int i = 0; i < 4; i++)
+            {
+                int x = r1 + dir2[i], y = c1 + dir2[i + 1];
+                if ((x >= 0 and x < row) and (y >= 0 and y < col) and grid[x][y] == 1 and visited[x][y] == 0)
+                {
+                    q.push({x, y});
                     visited[x][y] = 1;
                 }
             }
             ctr++;
         }
         return ctr;
-        
     }
-    int findMaxArea(vector<vector<int>>& grid) {
+    int findMaxArea(vector<vector<int>> &grid)
+    {
         // Code here
         row = grid.size(), col = grid[0].size();
         int res = 0;
         // queue<pair<int,int>>q;
-        vector<vector<int>>visited(row,vector<int>(col,0));
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++){
-                if(grid[i][j] == 1 and visited[i][j] == 0){
-                    res = max(res,dfs(grid,visited,i,j));
+        vector<vector<int>> visited(row, vector<int>(col, 0));
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (grid[i][j] == 1 and visited[i][j] == 0)
+                {
+                    res = max(res, dfs(grid, visited, i, j));
                     // q.push({i,j});
                 }
             }
@@ -55,6 +64,6 @@ class Solution
 
 int main()
 {
-
-return 0;
+    // DONE
+    return 0;
 }
